d78d3aa74af583b556054b66b88edd4cebc4236d
Rafall9 <rafall9999983@gmail.com>
2022-03-03 20:00:01 +0100

    dodaj funkcje dzielenia

---
+++ podziel.py
@@ -0,0 +1,6 @@
+def podziel(var1, var2):
+    wynik = var1 / var2
+    print("Wynik dzielenia: " + str(wynik))
+
+
+podziel(50,2)
---
+++ pomnoz.py
@@ -0,0 +1,6 @@
+def odejmij(var1, var2):
+    wynik = var1 - var2
+    print("Wynik odejmowania: " + str(wynik))
+
+
+odejmij(50,2)
